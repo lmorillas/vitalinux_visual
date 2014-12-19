f1a4a655aad6806b602a884dd54a1bf761c846bc
lmorillas <morillas@gmail.com>
2014-12-19 13:42:54 +0100

    datos vitalinux

--- datos_ordena.py
+++ datos_ordena.py
@@ -2,15 +2,16 @@
 
 
 from migasfree.server.models import Computer
+from django.db.models import Max
 
 BUSCADOS = 'memory processor disk'.split()
 
 
 def busca_datos(co):
     datos = {'id': co.id}
-    datos{'memoria':  c.hwnode_set.filter(classname='memory').aggregate(Max('size')).get('size__max')}
-    datos{'procesador'}:  c.hwnode_set.filter(classname='processor')
-    datos{'disco'}:  [x.size for x in  c.hwnode_set.filter(classname='disk') if x.size ]
+    datos['memoria'] = co.hwnode_set.filter(classname='memory').aggregate(Max('size')).get('size__max')
+    datos['procesador'] =  [p.product for p in co.hwnode_set.filter(classname='processor') if p.product is not None]
+    datos['disco'] = [x.size for x in  co.hwnode_set.filter(classname='disk') if x.size ]
     return datos
 
 
